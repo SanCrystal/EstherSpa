fa6067b76d026db4a27188915091e29c0020e300
SanCrystal <anyanwu.amanzearthur@gmail.com>
2023-07-08 14:47:28 +0100

    minified assets

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,16 +1,14 @@
-
+import { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation,Pagination } from "swiper";
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/autoplay";
+import {BsFillArrowLeftCircleFill,BsFillArrowRightCircleFill} from "react-icons/bs";
+
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
+// import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import {BsFillArrowLeftCircleFill,BsFillArrowRightCircleFill} from "react-icons/bs";
+import "swiper/css/autoplay";
+
 import Spa from "../../assets/spa.jpg";
 import Spa1 from "../../assets/spa1.jpg";
 import Spa2 from "../../assets/spa2.jpg";
@@ -20,14 +18,65 @@ import Spa5 from "../../assets/spa5.jpg";
 import Spa6 from "../../assets/spa6.jpg";
 import ImageSwiper from "../imageSwiper";
 
+const images = [
+  {
+    image:Spa,
+    header:"Welcome to Four Point Spa - Where Bliss Awaits",
+    subheader:"",
+    text:"Discover a World of Tranquility and Rejuvenation",
+  },
+  {
+    image:Spa1,
+    header:"Welcome to Four Point Spa - Where Bliss Awaits",
+    subheader:"",
+    text:"Discover a World of Tranquility and Rejuvenation",
+  },
+  {
+    image:Spa2,
+    header:"Welcome to Four Point Spa - Where Bliss Awaits",
+    subheader:"",
+    text:"Discover a World of Tranquility and Rejuvenation",
+  },
+  {
+    image:Spa3,
+    header:"Welcome to Four Point Spa - Where Bliss Awaits",
+    subheader:"",
+    text:"Discover a World of Tranquility and Rejuvenation",
+  },
+  {
+    image:Spa4,
+    header:"Welcome to Four Point Spa - Where Bliss Awaits",
+    subheader:"",
+    text:"Discover a World of Tranquility and Rejuvenation",
+  },
+  {
+    image:Spa5,
+    header:"Welcome to Four Point Spa - Where Bliss Awaits",
+    subheader:"",
+    text:"Discover a World of Tranquility and Rejuvenation",
+  },
+  {
+    image:Spa6,
+    header:"Welcome to Four Point Spa - Where Bliss Awaits",
+    subheader:"",
+    text:"Discover a World of Tranquility and Rejuvenation",
+  }
+]
+
 
 const Hero = () => {
-
+    const prevRef = useRef(null);
+    const nextRef = useRef(null);
     return ( <>
     <section className="relative">
-    <div className="nav absolute z-10 flex justify-between w-full top-[50%]">
-        <BsFillArrowLeftCircleFill   className="swiper-button-prev text-secondaryLight text-3xl"/>
-        <BsFillArrowRightCircleFill className="swiper-button-next text-secondaryLight text-3xl"/>
+    <div className="nav absolute z-20 flex justify-between w-full top-[50%]">
+      <div ref={prevRef}>
+           <BsFillArrowLeftCircleFill    className="swiper-button-prev text-secondaryLight text-3xl cursor-pointer"/>
+      </div>
+       <div ref={nextRef} >
+              <BsFillArrowRightCircleFill className="swiper-button-next text-secondaryLight text-3xl cursor-pointer"/>
+       </div>
+        
     </div>
     <Swiper
           slidesPerView={1}
@@ -36,42 +85,42 @@ const Hero = () => {
             601: { slidesPerView: 1 },
             1025: { slidesPerView: 1 },
           }}
+          loop={true}
+
           navigation={{
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            prevEl: prevRef.current!,
+            nextEl: nextRef.current!,
+            // nextEl: ".swiper-button-next",
+            // prevEl: ".swiper-button-prev",
             enabled: true,
           }}
+          onSwiper={(swiper) => {
+            // Delay execution for the refs to be defined
+            setTimeout(() => {
+              // Override prevEl & nextEl now that refs are defined
+              if(typeof swiper.params?.navigation != "boolean"){
 
-          
+                swiper.params.navigation!.prevEl = prevRef.current
+                swiper.params.navigation!.nextEl = nextRef.current
+                // Re-init navigation
+                swiper.navigation.destroy()
+                swiper.navigation.init()
+                swiper.navigation.update()
+              }
+    
+            },500)
+          }}
           pagination={{
             clickable: true,
           }}
           modules={[Pagination, Navigation, Autoplay]}
           autoplay={true}
           className="mySwiper"
+          
         >
-{/*          
-            
-            <SwiperSlide ><img src={Spa1} />  </SwiperSlide>
-            <SwiperSlide ><img src={Spa2} />  </SwiperSlide>
-            <SwiperSlide ><img src={Spa3} />  </SwiperSlide>
-            <SwiperSlide ><img src={Spa4} />  </SwiperSlide>
-            <SwiperSlide ><img src={Spa5} />  </SwiperSlide>
-            <SwiperSlide ><img src={Spa6} />  </SwiperSlide> */}
-            <SwiperSlide > <ImageSwiper src={Spa} position={"left"} header={"Welcome to Four Point Spa - Where Bliss Awaits"} subheader={""} text={"Discover a World of Tranquility and Rejuvenation"}/>  </SwiperSlide>
-           
-            <SwiperSlide > <ImageSwiper src={Spa1} position={"right"} header={"Welcome to Four Point Spa - Where Bliss Awaits"} subheader={""} text={"Discover a World of Tranquility and Rejuvenation"}/>  </SwiperSlide>
-           
-            <SwiperSlide > <ImageSwiper src={Spa2} position={"left"} header={"Welcome to Four Point Spa - Where Bliss Awaits"} subheader={""} text={"Discover a World of Tranquility and Rejuvenation"}/>  </SwiperSlide>
-           
-            <SwiperSlide > <ImageSwiper src={Spa3} position={"right"} header={"Welcome to Four Point Spa - Where Bliss Awaits"} subheader={""} text={"Discover a World of Tranquility and Rejuvenation"}/>  </SwiperSlide>
-           
-            <SwiperSlide > <ImageSwiper src={Spa4} position={"left"} header={"Welcome to Four Point Spa - Where Bliss Awaits"} subheader={""} text={"Discover a World of Tranquility and Rejuvenation"}/>  </SwiperSlide>
-            <SwiperSlide > <ImageSwiper src={Spa5} position={"right"} header={"Welcome to Four Point Spa - Where Bliss Awaits"} subheader={""} text={"Discover a World of Tranquility and Rejuvenation"}/>  </SwiperSlide>
-            <SwiperSlide > <ImageSwiper src={Spa6} position={"left"} header={"Welcome to Four Point Spa - Where Bliss Awaits"} subheader={""} text={"Discover a World of Tranquility and Rejuvenation"}/>  </SwiperSlide>
-           
-
- 
+            {
+              images.map((item:{image:string,header:string,subheader:string,text:string},k:number)=><SwiperSlide > <ImageSwiper src={item.image} position={k%2==0?"left":"right"} header={item.header} subheader={item.subheader} text={item.text}/>  </SwiperSlide>)
+            }
         </Swiper>
     </section>
     </> );
--- src/pages/home/testimonial.tsx
+++ src/pages/home/testimonial.tsx
@@ -13,10 +13,12 @@ const Testimonial = () => {
         <section className="pt-36 mx-5 md:mx-16 ">
             <h1 className="font-pacifico text-5xl text-primary pb-3 font-extralight text-center">Testimonial</h1>
             <h2 className="text-2xl uppercase text-primary text-center  pb-16">Customer Reviews</h2>
-            <div className="card-products place-items-center gap-6 justify-around relative">
-
+            <div className="card-products flex place-items-center gap-6 justify-around relative">
+            <div className="flex justify-between place-items-center absolute w-full z-10">
             <BsFillArrowLeftCircleFill  className="swiper-button-prev text-secondaryLight text-3xl"/>
             <BsFillArrowRightCircleFill className="swiper-button-next text-secondaryLight text-3xl"/>
+            </div>
+
             
                 <Swiper
                 slidesPerView={1}
